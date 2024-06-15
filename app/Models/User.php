0f32f9f15dd56b0a5b47c9bc064ff69912e00ed2
wisdom1 <126677054+eng-wisdom@users.noreply.github.com>
2024-06-15 10:24:27 +0000

    Installing and initializing dependencies

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Dyrynda\Database\Support\GeneratesUuid;
+use Dyrynda\Database\Casts\EfficientUuid;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, GeneratesUuid;
 
     /**
      * The attributes that are mass assignable.
@@ -40,5 +42,6 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'uuid' => EfficientUuid::class,
     ];
 }
--- config/app.php
+++ config/app.php
@@ -186,6 +186,8 @@ return [
          * Package Service Providers...
          */
 
+         Dyrynda\Database\LaravelEfficientUuidServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
